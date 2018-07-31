cc1f4658f695803d5d3a20bf443d355ae17d0242
AmitThakkar <vigildbest@gmail.com>
2018-07-31 16:52:25 +0200

    reading collection name from query string.

--- src/api/collection/list-product-by-collection-name.js
+++ src/api/collection/list-product-by-collection-name.js
@@ -6,15 +6,18 @@ exports.handler = function(event, context, callback) {
         TableName : process.env.TableName,
     };
 
-    if(true) {
-        params.FilterExpression = "#collection_name = :collection_name";
-        params.ExpressionAttributeNames = {
-            "#collection_name": "collection_name"
-        };
-        params.ExpressionAttributeValues = {
-            ":collection_name": "classic-petite"
-        };
+    const CollectionName = event.collection_name;
+
+    if(!CollectionName) {
+        callback(new Error("Collection name is missing into query string!"));
     }
+    params.FilterExpression = "#collection_name = :collection_name";
+    params.ExpressionAttributeNames = {
+        "#collection_name": "collection_name"
+    };
+    params.ExpressionAttributeValues = {
+        ":collection_name": CollectionName
+    };
 
     DocumentClient.scan(params, function(err, data) {
        if(err) callback(err);
